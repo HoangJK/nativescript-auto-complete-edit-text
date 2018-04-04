9bd8c7d30952e9317f6d741c931e4ad2f31319f9
Hoang Le <hoangle@innoria.com>
2018-04-04 09:02:47 +0700

    update demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,5 +1,4 @@
 import { Observable } from 'tns-core-modules/data/observable';
-import { AutoCompleteTextField } from 'nativescript-auto-complete-text-field';
 
 export class HelloWorldModel extends Observable {
 
